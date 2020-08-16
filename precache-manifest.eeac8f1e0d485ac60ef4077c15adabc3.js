a45ce0fbbaa5c3350f44205cbfad1a45c5ae231a
Artem Meges <artemmeges@gmail.com>
2020-08-16 20:30:15 +0300

    Updates

---
+++ precache-manifest.eeac8f1e0d485ac60ef4077c15adabc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea0e7642c4aae30e2789d62c5529ccb",
+    "url": "/react-expenses-calculator/index.html"
+  },
+  {
+    "revision": "5ceb03b420d5191f8aa9",
+    "url": "/react-expenses-calculator/static/css/main.d850324e.chunk.css"
+  },
+  {
+    "revision": "a1a0c71ca47b556fece9",
+    "url": "/react-expenses-calculator/static/js/2.dac34834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expenses-calculator/static/js/2.dac34834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ceb03b420d5191f8aa9",
+    "url": "/react-expenses-calculator/static/js/main.7fd928d5.chunk.js"
+  },
+  {
+    "revision": "526ed5d8fc1e934ca618",
+    "url": "/react-expenses-calculator/static/js/runtime-main.14e74239.js"
+  }
+]);
